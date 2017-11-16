0cf2ed7d00c9e23fd17e22564edf8905242f41be
Eugenawa, Cds (Ext) <Cds.Eugenawa-e@caissedesdepots.fr>
2017-11-16 16:20:22 +0100

    Cimmit

---
+++ src/app/leaves/model/leave.model.ts
@@ -0,0 +1,8 @@
+export class LeaveModel {
+   constructor(
+       private startDate: string,
+       private endDate: string,
+       private type: string,
+       private description: string){}
+
+}
---
+++ src/app/leaves/services/leaves.service.ts
@@ -0,0 +1,26 @@
+
+import {LeaveModel} from "../model/leave.model";
+import {Subject} from "rxjs";
+export class LeaveService {
+
+   leavesChanged = new Subject<LeaveModel[]>()
+
+   leaves: LeaveModel [] = [
+        new LeaveModel('12-12-2017','12-12-2018','CM', 'Holidays In France'),
+        new LeaveModel('21-02-2018','28-02-2018','CP','Holidays at the ski' )
+
+    ];
+
+    getLeaves(){
+        return this.leaves.slice();
+    }
+
+    getLeave(index: number){
+        return this.leaves[index];
+    }
+
+    deleteLeave(index: number){
+        this.leaves.splice(index, 1);
+        this.leavesChanged.next(this.leaves.slice());
+    }
+}
